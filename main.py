18eb3c408d33a06edf8a967b7c4de550b5a65465
Nate C-K <ncmadcity@gmail.com>
2026-05-19 09:07:08 -0500

    Working on fetching

--- main.py
+++ main.py
@@ -6,9 +6,27 @@ import requests
 
 NECESSE_CONSUMABLES_PAGE = "https://necessewiki.com/Consumables"
 
+def parse_page_name(url):
+    u = url.split('#')[0]
+    u = url.split('?')[0]
+    return u.split('/')[-1]
+
 def get(url):
+    page_name = parse_page_name(url)
+    path = "scraped/" + page_name
+    try:
+        with open(path, "r", encoding="utf8") as f:
+            print("Retrieved from cache:", page_name)
+            return f.read()
+    except:
+        pass # continue from here
     print("Fetching URL:", url)
-    return requests.get(NECESSE_CONSUMABLES_PAGE)
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f"Unable to fetch page ({reponse.status_code}): {url}")
+    with open(path, "w", encoding="utf8") as f:
+        f.write(response.text)
+    return reponse.text
 
 def main():
     consumables_page = get(NECESSE_CONSUMABLES_PAGE)
